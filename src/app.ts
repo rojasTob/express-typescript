e3a809dcf83ddef28accf9a7ccf535b23347702b
Angie Rojas <nataliart4@gmail.com>
2024-02-01 13:26:30 -0500

    Set connection with the data base

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,32 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import { PrismaClient } from '@prisma/client';
+
+export const prisma = new PrismaClient();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
-export const server = app.listen(PORT,()=>{
-  console.log(`Server is listening at ${PORT} port.`)
-});
+async function main(){
+  app.use(express.json());
 
-server.on("error", console.error);
+  app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World + prisma connection!');
+  });
 
+  app.listen(PORT,()=>{
+    console.log(`Server is listening at ${PORT} port.`)
+  });
+  
+}
+
+main()
+  .then(async () => {
+    await prisma.$connect();
+    console.info("Connected successfully with the db.")
+  })
+  .catch(async (e) => {
+    console.error(`Cannot connect with prisma ${e}`);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
