dc5310abae6e02fded305dd23bbc45f542c6d810
Angie Rojas <nataliart4@gmail.com>
2024-06-24 16:56:02 -0500

    To save and list authors

--- client/src/pages/Authors.tsx
+++ client/src/pages/Authors.tsx
@@ -1,10 +1,33 @@
-import React, {useState} from "react";
-import {Box, Heading, Input, Button, useToast} from "@chakra-ui/react";
-import {saveAuthor} from "../services/authors";
+import React, {useState, useEffect} from "react";
+import {Flex, Box, Heading, Input, Button, useToast, TableContainer, Table, Thead, Tbody, Th, Tr, Td} from "@chakra-ui/react";
+import {saveAuthor, getAuthors} from "../services/authors";
+import { AuthorData } from "../types";
+import { getAllByTestId } from "@testing-library/react";
 
 const Authors = () => {
   const [authorData, setAuthorData] = useState({fullname: '', biography: '', image: ''});
+  const [authors, setAuthors] = useState(Array<AuthorData>);
   const toast = useToast();
+
+  const getAuthorsList = async() => {
+    try{
+      const response = await getAuthors();
+      setAuthors(response);
+    }catch(error){
+      console.error('Can not get authors list');
+      toast({
+        title: 'Authors list error',
+        description: 'Unable to get the authors list',
+        status: 'error',
+        duration: 5000,
+        isClosable: true
+      });
+    }
+  }
+
+  useEffect(() => {
+    getAuthorsList();
+  }, []);
 
   const handleChange = (event: React.ChangeEvent <HTMLInputElement | HTMLTextAreaElement>) => {
     const {name, value} = event.target;
@@ -41,15 +64,39 @@ const Authors = () => {
   };
 
   return (
-    <Box  pl={10} pr={10}>
-      <Heading>Authors page</Heading>
-      <form className="form" onSubmit={handleSubmit}>
-        <Input className="input-field" placeholder="Fullname" isRequired={true} name="fullname" value={authorData.fullname} onChange={handleChange}/>
-        <Input className="input-field" placeholder="Biography" isRequired={true} name="biography" value={authorData.biography} onChange={handleChange}/>
-        <Input className="input-field" placeholder="URL image" name="image" value={authorData.image} onChange={handleChange}/>
-        <Button className="btn-1" type="submit">Add</Button>
-      </form>
-    </Box>
+    <Flex direction={"column"}>
+      <Box mt={4} mb={4}>
+        <Heading>Authors page</Heading>
+        <form className="form" onSubmit={handleSubmit}>
+          <Input className="input-field" placeholder="Fullname" isRequired={true} name="fullname" value={authorData.fullname} onChange={handleChange}/>
+          <Input className="input-field" placeholder="Biography" isRequired={true} name="biography" value={authorData.biography} onChange={handleChange}/>
+          <Input className="input-field" placeholder="URL image" name="image" value={authorData.image} onChange={handleChange}/>
+          <Button className="btn-1" type="submit">Add</Button>
+        </form>
+      </Box>
+      <Box mt={4} mb={4}>
+        <TableContainer>
+          <Table variant='simple'>
+            <Thead>
+              <Tr>
+                <Th>Author</Th>
+                <Th>Options</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {
+                authors.map( author => (
+                  <Tr>
+                    <Td>{author.fullname}</Td>
+                    <Td>Edit | Remove</Td>
+                  </Tr>
+                ))
+              }
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Flex>
   );
 };
 
--- client/src/services/authors.ts
+++ client/src/services/authors.ts
@@ -1,17 +1,31 @@
-const API_URL = "http://localhost:3001/api/";
+import {AuthorData} from "../types";
 
-interface AuthorData {
-  fullname: string;
-  biography: string;
-  image: string;
-}
+const API_URL = "http://localhost:3001/api/author";
 
 export const saveAuthor = async (authorData: AuthorData) => {
   try{
-    const response = await fetch(API_URL + 'author/create', {
+    const response = await fetch(API_URL + '/create', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(authorData)
+    });
+
+    if(!response.ok){
+      throw new Error('Network response was not ok');
+    }
+
+    const result = await response.json();
+    return result;
+  }catch(error){
+    console.error('Author fecthing error: ', error);
+  }
+}
+
+export const getAuthors = async () => {
+  try{
+    const response = await fetch(API_URL + '/getAll', {
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'}
     });
 
     if(!response.ok){
